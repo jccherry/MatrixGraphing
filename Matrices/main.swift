d4a624d3a973dc8bc23c02cb3c253bd32b546e02
jccherry <jchiaramonte18@regis.org>
2017-02-15 22:33:50 -0500

    removed remnants of old project

--- Matrices/main.swift
+++ Matrices/main.swift
@@ -8,56 +8,6 @@
 
 import Foundation
 
-let matrixArray: [[Double]] = [
-
-    [2,4,2,-1],
-    [2,5,1,6],
-    [1,2,0,1]
-
-]
-
-let refArray: [[Double]] = [
-
-    [1.0, 2.0, 0.0, 1.0],
-    [0.0, 1.0, 1.0, 4.0],
-    [0.0, 0.0, 1.0, -1.5]
-
-]
-
-/*
- 3x -5y +2z +9w +2v = -7
- x  +4y -z  +3w -v  = 10
- 6x +7y -z  -4w +5v = -18
- 5x +9y +3z +6w -v  = -5
- 2x -y  +5z -w  +8v = 12
- */
-
-let fiveBySix: [[Double]] = [
-
-    [3,-5,2,9,2,-7],
-    [1,4,-1,3,-1,10],
-    [6,7,-1,-4,5,-18],
-    [5,9,3,6,-1,-5],
-    [2,-1,5,-1,8,12]
-
-]
-
-//print("Welcome to the Matrix RREF calculator. \n Enter how many rows in a n x (n+1) matrix to solve the system.")
-
-//let rowNum = Int(readLine()!)
-
-
-let matrix = Matrix(rows: 5, columns: 6)
-matrix.rowsArray = fiveBySix
-
-matrix.rref()
-//matrix.printMatrix()
-
-
-
-
-
-
 let testMatrix = Matrix(rows: 30, columns: 100)
 sinGraph(amplitude: 5, horizStretch: 1/10, horizShift: -Int(M_PI/2), vertShift: 10, graphMatrix: testMatrix)
 expGraph(coefficient: 1/175, power: 3, horizShift: 40, vertShift: 10, graphMatrix: testMatrix)
